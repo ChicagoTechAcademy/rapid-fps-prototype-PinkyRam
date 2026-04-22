a75b32a97dadefb5c250ab94fe46a7ac48ddda75
PinkyRam <ljperez8@chicagotechacademy.org>
2026-04-22 11:18:01 -0500

    Half way done with Video 2

--- Source/RapidFPS/FPSProjectile.h
+++ Source/RapidFPS/FPSProjectile.h
@@ -5,6 +5,7 @@
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
 #include "Components/SphereComponent.h"
+#include "GameFramework/ProjectileMovementComponent.h"
 #include "FPSProjectile.generated.h"
 
 UCLASS()
@@ -20,8 +21,21 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
+
+	// Sphere collision component.
+	UPROPERTY(VisibleDefaultsOnly, Category = Projectile)
+	USphereComponent* CollisionComponent;
+
+	// Projectile movement component.
+	UPROPERTY(VisibleAnywhere, Category = Movement)
+	UProjectileMovementComponent* ProjectileMovementComponent;
+
+	// Function that initializes the projectile's velocity in the shoot direction.
+	void FireInDirection(const FVector& ShootDirection);
+
 
 };
