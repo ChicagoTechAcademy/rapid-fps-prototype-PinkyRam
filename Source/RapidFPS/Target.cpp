bebdcae6d06e7f06b03e8c075f544688ded79b20
PinkyRam <ljperez8@chicagotechacademy.org>
2026-05-13 11:23:01 -0500

    3/4 Done with Video 6

---
+++ Source/RapidFPS/Target.cpp
@@ -0,0 +1,52 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Target.h"
+#include "Components/StaticMeshComponent.h"
+#include "Components/SphereComponent.h"
+
+// Sets default values
+ATarget::ATarget()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	Collision = CreateDefaultSubobject<USphereComponent>(TEXT("Sphere Collision"));
+	SetRootComponent(Collision);
+
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Static Mesh"));
+	Mesh->SetupAttachment(RootComponent);
+
+	Collision->OnComponentHit.AddDynamic(this, &ATarget::OnHit);
+
+
+
+}
+
+// Called when the game starts or when spawned
+void ATarget::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ATarget::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void ATarget::OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
+{
+	if (OtherActor && (OtherActor != this) && OtherComp)
+	{
+		// Handle logic here, e.g., print the name of the hit actor
+//		UE_LOG(LogTemp, Warning, TEXT("Hit: %s"), OtherActor->GetName());
+
+	}
+
+}
+
+
+
